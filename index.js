bf242eb98da2887da8c9b13cf51a0883b2089438
Hugocod <u.medioli@icloud.com>
2022-09-05 09:29:30 +0200

    added bonus

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ createGridBtn.addEventListener("click", createNewGrid);
 
 function createNewGrid() {
     let cellContainer = document.getElementById("grid-container");
-
     cellContainer.innerHTML = "";
-    for (let index = 1; index <= 100; index++) {
+    let list = createRandomNumberList();
+    console.log(list);
+
+    for (let index = 0; index < 100; index++) {
         let cell = document.createElement("div");
+        cell.innerHTML = `${list[index]}`;
+        cellContainer.appendChild(cell);
 
         cell.addEventListener("mouseover", () => {
-            if (index % 2 === 0) {
+            if (list[index] % 2 === 0) {
                 console.log("pari");
                 cell.classList.add("even");
             } else {
@@ -18,8 +22,20 @@ function createNewGrid() {
                 cell.classList.add("odd");
             }
         });
-
-        cell.innerHTML = `${index}`;
-        cellContainer.appendChild(cell);
     }
+}
+
+/* bonus */
+function createRandomNumberList() {
+    let numberList = [];
+
+    while (numberList.length < 100) {
+        let randomNumber = Math.floor(Math.random() * 100 + 1);
+
+        if (!numberList.includes(randomNumber)) {
+            numberList.push(randomNumber);
+        }
+    }
+
+    return numberList;
 }
